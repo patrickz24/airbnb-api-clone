b69816d77241909404e17b773bbd94e23321143d
patrickz24 <patrick.zamblebi@yahoo.fr>
2020-08-27 12:43:09 +0200

    erreur + refacto place user

--- routes/userCtrl.js
+++ routes/userCtrl.js
@@ -1,72 +1,101 @@
+const express = require('express');
+require('express-async-errors');
 const bcrypt = require('bcrypt');
 const models = require('../models');
 const jwtUtils = require('../utils/jwt.utils');
+const { OK } = require('../helpers/status_codes');
+const {
+  BadRequestError,
+  ServerError,
+  ConflictError,
+  ForbiddenError,
+} = require('../helpers/errors');
 
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const PASSWORD_REGEX = /^(?=.*\d).{4,8}$/;
 const FIRSTNAME_REGEX = /^[a-zA-Z]{1,}$/;
 
 module.exports = {
-  signup: function (req, res) {
+  signup: async function (req, res) {
     const { email, first_name, last_name, password, role } = req.body;
 
     if (first_name == null || last_name == null) {
-      return res.status(400).json({ error: 'missing parameters' });
+      throw new BadRequestError(
+        'Mauvaise Requête',
+        'Les champs first name et/ou name ne sont pas renseignés , veuillez recommencer.'
+      );
     }
 
     if (!EMAIL_REGEX.test(email)) {
-      return res.status(400).json({ error: 'email is not valid' });
+      throw new BadRequestError(
+        'Mauvaise Requête',
+        "L'e-mail n'est pas valide, veuillez recommencer."
+      );
     }
 
     if (!PASSWORD_REGEX.test(password)) {
-      return res
-        .status(400)
-        .json({ error: 'password invalid (must length 4 - 8 and include 1 number at least)' });
+      throw new BadRequestError(
+        'Mauvaise Requête',
+        'Mot de passe invalide (doit avoir une longueur de 4 à 8 caractères et inclure au moins un chiffre), veuillez recommencer.'
+      );
     }
 
     if (!FIRSTNAME_REGEX.test(first_name)) {
-      return res.status(400).json({ error: 'il faut une chaine de caractères' });
+      throw new BadRequestError(
+        'Mauvaise Requête',
+        'Le champ first name doit être une chaîne de caractères'
+      );
     }
 
-    models.User.findOne({
+    const userFound = await models.User.findOne({
       attributes: ['email'],
       where: { email: email },
     })
-      .then(function (userFound) {
+  
         if (!userFound) {
           bcrypt.hash(password, 5, function (err, bcryptedPassword) {
-            const newUser = models.User.create({
+            const newUser = await models.User.create({
               email: email,
               first_name: first_name,
               last_name: last_name,
               password: bcryptedPassword,
               role: role,
             })
-              .then(function (newUser) {
-                return res.status(200).json({
+              if (!newUser) {
+                throw new ServerError('Erreur Serveur', "Impossible d'ajouter cet utilisateur.");
+              } else {
+                return res.status(OK).json({
                   email: newUser.email,
                   first_name: newUser.first_name,
                   last_name: newUser.last_name,
                   role: newUser.role,
                 });
-              })
-              .catch(function (err) {
-                return res.status(500).json({ error: 'cannot add user' });
-              });
+              
+              }
+                
+              // .catch(function (err) {
+              //   throw new ServerError('Erreur Serveur', "Impossible d'ajouter cet utilisateur.");
+              // });
           });
         } else {
-          return res.status(409).json({ error: 'user already exist' });
+          console.log('hello');
+          throw new ConflictError('Conflit', 'Cet utilisateur existe déjà.');
+          // return res.status(409).json({ error: 'user already exist' });
         }
-      })
-      .catch(function (err) {
-        return res.status(500).json({ error: 'unable to verify user' });
-      });
+    
+      // .catch(function (err) {
+      //   console.log('haha');
+      //   throw new ServerError('Erreur Serveur', 'Impossible de vérifier cet utilisateur.');
+      // });
   },
   signin: function (req, res) {
     const { email, password } = req.body;
-console.log(req.body)
+    console.log(req.body);
     if (email == null || password == null) {
-      return res.status(400).json({ error: 'missing parameters' });
+      throw new BadRequestError(
+        'Mauvaise Requête',
+        'Les champs e-mail et/ou mot de passes sont manquants, veuillez recommencer.'
+      );
     }
 
     models.User.findOne({
@@ -76,9 +105,9 @@ console.log(req.body)
         if (userFound) {
           bcrypt.compare(password, userFound.password, function (errBycrypt, resBycrypt) {
             if (resBycrypt) {
-              return res.status(200).json({
+              return res.status(OK).json({
                 user: {
-                  id : userFound.id,
+                  id: userFound.id,
                   email: userFound.email,
                   first_name: userFound.first_name,
                   last_name: userFound.last_name,
@@ -87,15 +116,22 @@ console.log(req.body)
                 token: jwtUtils.generateTokenForUser(userFound),
               });
             } else {
-              return res.status(403).json({ error: 'invalid password' });
+              // return res.status(403).json({ error: 'invalid password' });
+              throw new ForbiddenError(
+                'Accès refusé',
+                'Le mot de passe est incorrect, veuillez recommencer.'
+              );
             }
           });
         } else {
-          return res.status(400).json({ error: 'user not exist in DB airbnb_api' });
+          throw new BadRequestError(
+            'Mauvaise Requête',
+            "Cet utilisateur n'existe pas dans la DB airbnb_api"
+          );
         }
       })
       .catch(function (err) {
-        return res.status(500).json({ error: 'unable to verify user' });
+        throw new ServerError('Erreur Serveur', 'Impossible de vérifier cet utilisateur.');
       });
   },
 };
